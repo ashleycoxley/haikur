9e85c435c87783540356f3e45c7960256e56e8d1
ashleycoxley <ashleycoxley@gmail.com>
2016-10-06 17:35:02 -0700

    Added basic show/hide comments
     - javascript to show/hide
     - overflow scrolling in comment section

--- js/comment.js
+++ js/comment.js
@@ -1,27 +1,40 @@
 var commentTemplate = "<div class='comment-box'><p>username</p><p>comment_text</p></div>"
 
-$(document).ready( function() {
+function buildComment(commentTemplate, username, commentText) {
+	var newComment = commentTemplate
+			.replace('username', username)
+			.replace('comment_text', commentText);
+	return newComment
+};
+
+$(document).ready(function() {
 	$('.comment-form').on('submit', function(event) {
 		event.preventDefault();
+
 		var commentText = $(this).find('.new-comment-text').val();
 		var haikuID = $(this).parent().attr('data-haikuid');
+		var username = $('.menu-box h3').text();
 
 		var data = {
 			'commentText': commentText,
 			'haikuID': haikuID
 		};
 
-		var username = $('.menu-box h3').text();
-		var newComment = commentTemplate.replace('username', username).replace('comment_text', commentText);
 		var commentUrl = '/' + haikuID + '/comment';
 		var commentPost = $.post(commentUrl, data);
 
+		var newComment = buildComment(commentTemplate, username, commentText)
+
 		$(this).children('.comment-form-box').children('.new-comment-text').val('');
 		var commentList = $(this).parent().siblings('.comment-list');
-
 		commentPost.done(function() {
 			commentList.append(newComment);
-		});
+		});	
+	});
+
+	$('.comment-icon').on('click', function(e){
+  		e.preventDefault();
+  		$(this).parent().siblings('.comment-box').toggleClass('hide');
 	});
 });
 
